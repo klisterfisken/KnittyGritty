44064dc9a150734bc28aa6c9b0014d4a0f51ce4e
Hanna <206832485+klisterfisken@users.noreply.github.com>
2026-03-21 19:41:37 +0100

    Scaffold languages

---
+++ KnittyGritty/Pages/Languages/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KnittyGritty.Data;
+using KnittyGritty.Models;
+
+namespace KnittyGritty.Pages.Languages
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly KnittyGritty.Data.KnittyGrittyContext _context;
+
+        public DeleteModel(KnittyGritty.Data.KnittyGrittyContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Language Language { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var language = await _context.Language.FirstOrDefaultAsync(m => m.LanguageID == id);
+
+            if (language is not null)
+            {
+                Language = language;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var language = await _context.Language.FindAsync(id);
+            if (language != null)
+            {
+                Language = language;
+                _context.Language.Remove(Language);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ KnittyGritty/Pages/Languages/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KnittyGritty.Data;
+using KnittyGritty.Models;
+
+namespace KnittyGritty.Pages.Languages
+{
+    public class EditModel : PageModel
+    {
+        private readonly KnittyGritty.Data.KnittyGrittyContext _context;
+
+        public EditModel(KnittyGritty.Data.KnittyGrittyContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Language Language { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var language =  await _context.Language.FirstOrDefaultAsync(m => m.LanguageID == id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+            Language = language;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Language).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LanguageExists(Language.LanguageID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool LanguageExists(int id)
+        {
+            return _context.Language.Any(e => e.LanguageID == id);
+        }
+    }
+}
