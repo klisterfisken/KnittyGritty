ab190b48750039e81ceb32b2171e8936b590be34
Hanna <206832485+klisterfisken@users.noreply.github.com>
2026-03-24 21:30:20 +0100

    Add dark/light mode toggle button

--- KnittyGritty/wwwroot/js/site.js
+++ KnittyGritty/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.getElementById('toggleSwitch').addEventListener('click',()=>{
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark')
+    }
+})
