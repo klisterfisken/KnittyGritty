7c188fdef8683f59feadf4fd7f4735846edd05b7
Hanna <206832485+klisterfisken@users.noreply.github.com>
2026-04-02 17:02:13 +0200

    Add pagination to pattern index

--- KnittyGritty/Pages/Patterns/Index.cshtml.cs
+++ KnittyGritty/Pages/Patterns/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿
 using KnittyGritty.Data;
 using KnittyGritty.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,12 +18,34 @@ namespace KnittyGritty.Pages.Patterns
 
         public IList<Pattern> Pattern { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public int PageIndex { get; set; } = 1;
+
+        [BindProperty(SupportsGet = true)]
+        public int PageSize { get; set; } = 12;
+
+        public int TotalPages { get; set; }
+
         public async Task OnGetAsync()
         {
-            Pattern = await _context.Pattern
+            var validSizes = new[] { 12, 24, 48 };
+            if (!validSizes.Contains(PageSize)) PageSize = 12;
+
+            var query = _context.Pattern
                 .Include(p => p.Designer)
                 .Include(p => p.PatternCategories)
                     .ThenInclude(pc => pc.Category)
+                .AsNoTracking();
+
+            var total = await query.CountAsync();
+            TotalPages = (int)Math.Ceiling(total / (double)PageSize);
+            if (PageIndex < 1) PageIndex = 1;
+            if (PageIndex > TotalPages && TotalPages > 0) PageIndex = TotalPages;
+
+            Pattern = await query
+                .OrderBy(p => p.Title)
+                .Skip((PageIndex - 1) * PageSize)
+                .Take(PageSize)
                 .ToListAsync();
         }
     }
